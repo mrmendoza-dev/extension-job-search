d9140f97a724114bf4bf483341c02c39f7005699
Steven Mendoza <smendoza171@gmail.com>
2023-01-15 12:15:52 -0800

    made coillapsing sidebar

--- react-chrome-app/src/App.tsx
+++ react-chrome-app/src/App.tsx
@@ -137,6 +137,7 @@ function refreshFilter() {
         } else if (myJobsStateEl.innerText.toLowerCase().includes("visited")) {
           if (filters.visited) {
             divs[i].style.display = "block";
+
           } else {
             divs[i].style.display = "none";
           }
@@ -146,6 +147,7 @@ function refreshFilter() {
       if (indeedApplyEl) {
         if (filters.easilyApply) {
           divs[i].style.display = "block";
+
         } else {
           divs[i].style.display = "none";
         }
@@ -154,6 +156,7 @@ function refreshFilter() {
       if (urgentlyHiringEl) {
         if (filters.urgentlyHiring) {
           divs[i].style.display = "block";
+
         } else {
           divs[i].style.display = "none";
         }
@@ -162,6 +165,7 @@ function refreshFilter() {
       if (hiringMultipleCandidatesEl) {
         if (filters.hiringMultipleCandidates) {
           divs[i].style.display = "block";
+
         } else {
           divs[i].style.display = "none";
         }
@@ -170,6 +174,7 @@ function refreshFilter() {
       if (responsiveEmployerEl) {
         if (filters.responsiveEmployer) {
           divs[i].style.display = "block";
+
         } else {
           divs[i].style.display = "none";
         }
@@ -177,200 +182,218 @@ function refreshFilter() {
       if (smbD2iInterviewEl) {
         if (filters.easilyArrange) {
           divs[i].style.display = "block";
+
         } else {
           divs[i].style.display = "none";
         }
       }
 
-      // if (ratingNumberEl) {
-      //   if (Number(ratingNumberEl?.innerText) < filters.minRating) {
-      //     if (filters.company) {
-      //       divs[i].style.display = "block";
-      //     } else {
-      //       divs[i].style.display = "none";
-      //     }
-      //   }
-      // } else {
-      //   divs[i].style.display = "none";
-      // }
+
+      if (ratingNumberEl) {
+        if (Number(ratingNumberEl?.innerText) < filters.minRating) {
+          if (filters.company) {
+            divs[i].style.display = "block";
+          } else {
+            divs[i].style.display = "none";
+          }
+        }
+      } else {
+        divs[i].style.display = "none";
+      }
 
 
-      // if (companyLocationEl) {
-      //   if (filters.location) {
-      //     blacklist.forEach((keyword) => {
-      //       if (companyLocationEl?.innerText.toLowerCase().includes(keyword.toLowerCase())) {
-      //         divs[i].style.display = "none";
-      //       } else {
-      //         divs[i].style.display = "block";
-      //       }
-      //     });
-      //   }
-      // }
+      if (companyLocationEl) {
+        if (filters.location) {
+          blacklist.forEach((keyword) => {
+            if (companyLocationEl?.innerText.toLowerCase().includes(keyword.toLowerCase())) {
+              divs[i].style.display = "none";
+            } else {
+              divs[i].style.display = "block";
+            }
+          });
+        }
+      }
 
-      // if (dateEl?.innerText.includes("Posted")) {
+      if (dateEl?.innerText.includes("Posted")) {
 
-      // }
+      }
 
     }
   }
 
 
 
+    const [isToggled, setIsToggled] = useState(JSON.parse(localStorage.getItem("sidebarOpen") || "true"));
+    const handleClick = () => {
+      setIsToggled(!isToggled);
+    };
+
+      useEffect(() => {
+        localStorage.setItem("sidebarOpen", JSON.stringify(isToggled));
+      }, [isToggled]);
+
+
+
+
   return (
     <div className="App">
-      <header>
-        <h3 className="">Indeed Job Search Helper</h3>
-      </header>
+      <button className="collapse-btn" onClick={handleClick}>+</button>
 
-      <div className="filter-btns">
-        <button onClick={() => setIsActive(!isActive)}>
-          {isActive ? "Deactivate" : "Activate"}
-        </button>
+      <div id="my-element" className={isToggled ? "App-big" : "App-small"}>
+        <header>
+          <h3 className="">Indeed Job Search Helper</h3>
+        </header>
 
         <div className="filter-btns">
-          <label>
-            <input
-              type="checkbox"
-              name="jobTitle"
-              checked={filters.jobTitle}
-              onChange={handleFilterChange}
-            />
-            Job Title
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="company"
-              checked={filters.company}
-              onChange={handleFilterChange}
-            />
-            Company
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="location"
-              checked={filters.location}
-              onChange={handleFilterChange}
-            />
-            Location
-          </label>
+          <button onClick={() => setIsActive(!isActive)}>
+            {isActive ? "Deactivate" : "Activate"}
+          </button>
 
-          <label>
-            <input
-              type="checkbox"
-              name="saved"
-              checked={filters.saved}
-              onChange={handleFilterChange}
-            />
-            Show Saved Jobs
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="visited"
-              checked={filters.visited}
-              onChange={handleFilterChange}
-            />
-            Show Visited Jobs
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="responsiveEmployer"
-              checked={filters.responsiveEmployer}
-              onChange={handleFilterChange}
-            />
-            Responsive Employer
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="easilyApply"
-              checked={filters.easilyApply}
-              onChange={handleFilterChange}
-            />
-            Easily Apply
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="urgentlyHiring"
-              checked={filters.urgentlyHiring}
-              onChange={handleFilterChange}
-            />
-            Urgently Hiring
-          </label>
-          <label>
-            <input
-              type="checkbox"
-              name="hiringMultipleCandidates"
-              checked={filters.hiringMultipleCandidates}
-              onChange={handleFilterChange}
-            />
-            Hiring Multiple Candidates
-          </label>
+          <div className="filter-btns">
+            <label>
+              <input
+                type="checkbox"
+                name="jobTitle"
+                checked={filters.jobTitle}
+                onChange={handleFilterChange}
+              />
+              Job Title
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="company"
+                checked={filters.company}
+                onChange={handleFilterChange}
+              />
+              Company
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="location"
+                checked={filters.location}
+                onChange={handleFilterChange}
+              />
+              Location
+            </label>
 
-          <label>
-            <input
-              type="checkbox"
-              name="easilyArrange"
-              checked={filters.easilyArrange}
-              onChange={handleFilterChange}
-            />
-            Easily Arrange Interviews
-          </label>
+            <label>
+              <input
+                type="checkbox"
+                name="saved"
+                checked={filters.saved}
+                onChange={handleFilterChange}
+              />
+              Show Saved Jobs
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="visited"
+                checked={filters.visited}
+                onChange={handleFilterChange}
+              />
+              Show Visited Jobs
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="responsiveEmployer"
+                checked={filters.responsiveEmployer}
+                onChange={handleFilterChange}
+              />
+              Responsive Employer
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="easilyApply"
+                checked={filters.easilyApply}
+                onChange={handleFilterChange}
+              />
+              Easily Apply
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="urgentlyHiring"
+                checked={filters.urgentlyHiring}
+                onChange={handleFilterChange}
+              />
+              Urgently Hiring
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                name="hiringMultipleCandidates"
+                checked={filters.hiringMultipleCandidates}
+                onChange={handleFilterChange}
+              />
+              Hiring Multiple Candidates
+            </label>
 
-          <div className="filter-blacklist">
-            <input
-              type="text"
-              value={newItem}
-              onChange={(e) => setNewItem(e.target.value)}
-            />
-            <button onClick={handleAdd}>Add to Blacklist</button>
+            <label>
+              <input
+                type="checkbox"
+                name="easilyArrange"
+                checked={filters.easilyArrange}
+                onChange={handleFilterChange}
+              />
+              Easily Arrange Interviews
+            </label>
+
+            <div className="filter-blacklist">
+              <input
+                type="text"
+                value={newItem}
+                onChange={(e) => setNewItem(e.target.value)}
+              />
+              <button onClick={handleAdd}>Add to Blacklist</button>
+              <div>
+                <h3>Blacklist</h3>
+                <ul className="blacklist-box">
+                  {blacklist.map((item, index) => (
+                    <li key={item}>
+                      <button onClick={() => handleRemove(index)}>
+                        <i className="fa-regular fa-circle-xmark"></i>
+                      </button>
+                      {item}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
             <div>
-              <h3>Blacklist</h3>
-              <ul className="blacklist-box">
-                {blacklist.map((item, index) => (
-                  <li key={item}>
-                    <button onClick={() => handleRemove(index)}>
-                      <i className="fa-regular fa-circle-xmark"></i>
-                    </button>
-                    {item}
-                  </li>
-                ))}
-              </ul>
+              <input
+                type="range"
+                min={0}
+                max={5}
+                step={0.1}
+                value={filters.minRating}
+                onChange={handleChange}
+                name="minRating"
+              />
+              <p>Minimum Rating: {filters.minRating}</p>
+            </div>
+
+            <div>
+              <input
+                type="range"
+                min={0}
+                max={30}
+                step={1}
+                value={filters.maxDays}
+                onChange={handleChange}
+                name="maxDays"
+              />
+              <p>Maximum Days: {filters.maxDays}</p>
             </div>
           </div>
-
-          <div>
-            <input
-              type="range"
-              min={0}
-              max={5}
-              step={0.1}
-              value={filters.minRating}
-              onChange={handleChange}
-              name="minRating"
-            />
-            <p>Minimum Rating: {filters.minRating}</p>
-          </div>
-
-          <div>
-            <input
-              type="range"
-              min={0}
-              max={30}
-              step={1}
-              value={filters.maxDays}
-              onChange={handleChange}
-              name="maxDays"
-            />
-            <p>Maximum Days: {filters.maxDays}</p>
-          </div>
+          {/* <button onClick={() => window.location.reload()}>Reload</button> */}
+          <button onClick={filterPosts}>Refresh</button>
         </div>
-        {/* <button onClick={() => window.location.reload()}>Reload</button> */}
-        <button onClick={filterPosts}>Refresh</button>
       </div>
     </div>
   );
